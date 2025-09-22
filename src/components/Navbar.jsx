bbe2ff9b53137fa01dc8f5ecd30c1f0051f739f6
jesus santana <jesus.santanaalvarez12@gmail.com>
2025-09-22 17:34:25 +0200

    1.4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <nav className="navbar">
-      <ul>
-        <li><Link to="/">Inicio</Link></li>
-        <li><Link to="/experiencia">Experiencia</Link></li>
-        <li><Link to="/educacion">Educación</Link></li>
-        <li><Link to="/habilidades">Habilidades</Link></li>
-        <li><Link to="/idiomas">Idiomas</Link></li>
-        <li><Link to="/cursos">Cursos</Link></li>
-      </ul>
-    </nav>
+    <>
+      <header className="navbar">
+        <h1 className="logo">Mi Portfolio</h1>
+        <button className="menu-btn" onClick={() => setOpen(!open)}>
+          ☰
+        </button>
+      </header>
+
+      <aside className={`sidebar ${open ? "open" : ""}`}>
+        <button className="close-btn" onClick={() => setOpen(false)}>✕</button>
+        <ul>
+          <li><Link to="/" onClick={() => setOpen(false)}>Inicio</Link></li>
+          <li><Link to="/experiencia" onClick={() => setOpen(false)}>Experiencia</Link></li>
+          <li><Link to="/educacion" onClick={() => setOpen(false)}>Educación</Link></li>
+          <li><Link to="/habilidades" onClick={() => setOpen(false)}>Habilidades</Link></li>
+          <li><Link to="/idiomas" onClick={() => setOpen(false)}>Idiomas</Link></li>
+          <li><Link to="/cursos" onClick={() => setOpen(false)}>Cursos</Link></li>
+        </ul>
+      </aside>
+    </>
   );
 }
 
